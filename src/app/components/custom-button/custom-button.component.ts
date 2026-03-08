a49f6c5d954e8770385c1dc666ce50970c8b889c
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-08 16:22:03 +0100

    Add custom button component

---
+++ src/app/components/custom-button/custom-button.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  imports: [],
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.css',
+})
+export class CustomButtonComponent {
+  @Input() onClick?: () => void;
+  @Input() children?: string;
+}

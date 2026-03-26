c973e9e0fa452b595656c675db684be3d0740f8b
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-26 21:40:55 +0100

    feat: add generic not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {ShelfComponent} from './components/shelf/shelf.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: ShelfComponent },
-  { path: 's/:shelfId', component: ShelfComponent }
+  { path: 's/:shelfId', component: ShelfComponent },
+  { path: '**', component: NotFoundComponent }
 ];

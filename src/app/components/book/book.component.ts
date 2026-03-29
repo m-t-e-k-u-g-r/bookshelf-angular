e9b17c45be28b73c850b95ef7509a0fe68137927
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-03-29 15:36:58 +0200

    feat: add shelf management functionality

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,9 +1,9 @@
-import {Component, inject, Input} from '@angular/core';
+import {Component, ElementRef, inject, Input, signal, ViewChild} from '@angular/core';
 import {Book} from '../../models/book.type';
 import {NgOptimizedImage} from '@angular/common';
 import {KebabMenuComponent, MenuItem} from '../kebab-menu/kebab-menu.component';
 import {BookService} from '../../services/book.service';
-import {checkbox} from '@inquirer/prompts';
+import {ShelfService} from '../../services/shelf.service';
 
 @Component({
   selector: 'app-book',
@@ -31,13 +31,69 @@ import {checkbox} from '@inquirer/prompts';
           [items]="menuItems"
         />
       </div>
+      <dialog #shelfSelection>
+        <div>
+          <h3>Edit shelves for "{{book.title}}"</h3>
+          <div>
+            @for (shelf of shelfService.shelfNames(); track shelf) {
+              <div>
+                <label>
+                  <input
+                    #checkbox
+                    type="checkbox"
+                    [checked]="shelves().includes(shelf)"
+                    (change)="handleShelfChange(shelf, checkbox.checked)"
+                  />
+                  {{ shelf }}
+                </label>
+              </div>
+            }
+            <button (click)="handleSave()">
+              Close
+            </button>
+          </div>
+        </div>
+      </dialog>
     </div>
   `,
   styleUrl: './book.component.css',
 })
 export class BookComponent {
   @Input() book!: Book;
+  @ViewChild('shelfSelection') dialog!: ElementRef<HTMLDialogElement>;
   bookService = inject(BookService);
+  shelfService = inject(ShelfService);
+  shelves = signal<string[]>([]);
+
+  openShelfSelection() {
+    const shelfData = this.shelfService.shelvedBooks()
+      .filter(b => b.isbn === this.book.isbn)
+      .map(b => b.shelf);
+    this.shelves.set(shelfData);
+    this.dialog.nativeElement.showModal();
+  }
+
+  handleShelfChange(shelf: string, isChecked: boolean) {
+    if (isChecked) {
+      if (!this.shelves().includes(shelf)) this.shelves.set([...this.shelves(), shelf]);
+    } else {
+      this.shelves.set(this.shelves().filter(s => s !== shelf));
+    }
+  }
+
+  handleSave() {
+    this.shelfService.editShelvesOfBook(this.book.isbn, this.shelves())
+      .subscribe({
+        next: () => {
+          this.shelfService.getShelvedBooks();
+          this.shelfService.getSidebarData();
+          this.dialog.nativeElement.close();
+        },
+        error: (err) => {
+          console.error('Failed to save shelves', err);
+        }
+      });
+  }
 
   handleBookDelete() {
     this.bookService
@@ -52,7 +108,7 @@ export class BookComponent {
   }
 
   menuItems: MenuItem[] = [
-    {label: 'Delete', action: () => {this.handleBookDelete()}}
+    {label: 'Delete', action: () => {this.handleBookDelete()}},
+    {label: 'Edit shelves', action: () => {this.openShelfSelection()}}
   ]
-  protected readonly checkbox = checkbox;
 }
